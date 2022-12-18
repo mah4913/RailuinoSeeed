ee7d129e2f838d1d0aec0276ecafa36699af8bdd
Wolfgang Hammer <62503338+wphammer@users.noreply.github.com>
2022-12-18 15:43:31 +0100

    Change internal communication methods to use the MCP_CAN class. Remove unused stuff.

--- RailuinoSeeed.h
+++ RailuinoSeeed.h
@@ -1,7 +1,9 @@
 /*********************************************************************
- * Railuino - Hacking your Märklin
+ * RailuinoSeeed
+ * Adopted from: Railuino - Hacking your Märklin
  *
  * Copyright (C) 2012 Joerg Pleumann
+ * Copyright (C) 2022 Wolfgang Hammer
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -14,8 +16,8 @@
  * LICENSE file for more details.
  */
 
-#ifndef railuino__h
-#define railuino__h
+#ifndef RailuinoSeeed__h
+#define RailuinoSeeed__h
 
 #include <Arduino.h>
 #include <Printable.h>
@@ -157,99 +159,29 @@ class TrackMessage : public Printable {
    * undefined afterwards, and a clear() is recommended.
    */
   boolean parseFrom(String &s);
-
+  
+  /**
+   * Parses the message from the CAN message data returned by
+   * MCP_CAN::readMsgBufID().
+   */
+  boolean fromCanMsg(unsigned long aId, byte aExt, byte aRtr, byte aLen, byte *aBuf);
 };
 
-// ===================================================================
-// === TrackController ===============================================
-// ===================================================================
 
-/**
- * Controls things on and connected to the track: locomotives,
- * turnouts and other accessories. While there are some low-level
- * methods for dealing with messages, you will normally want to use
- * the high-level methods that wrap most of the the nasty protocol
- * details. When addressing something, you have to tell the system the
- * type of address (or decoder) you are using by adding the proper
- * protocol base address. For instance, DCC locomotive 42 is properly
- * addressed as ADDR_DCC + 42.
- */
+class MCP_CAN;
+
 class TrackController {
-
-    private:
-
-	/**
-	 * Stores the hash of our controller. This must not conflict with
-	 * hashes of other devices in the setup (most likely the MS2 and
-	 * the connector box).
-	 */
-	word mHash;
-
-	/**
-	 * Stores the debug flag. When debugging is on, all outgoing and
-	 * incoming messages are printed to the Serial console.
-	 */
-	boolean mDebug;
-	
-	/**
-	 * Holds the loopback flag. When loopback is on, messages are
-	 * reflected by the CAN controller. No external communication
-	 * takes place. This is helpful for some test cases.
-	 */
-	boolean mLoopback;
-
-	/**
-	 * Generates a new hash and makes sure it does not conflict
-	 * with those of other devices in the setup.
-	 */
-	void generateHash();
-
-    public:
-
-	/**
-	 * Creates a new TrackController with default values. This should
-	 * be fine for most use cases. Further configuration can be done
-	 * by using the init() method.
-	 */
-    TrackController();
-
-	/**
-	 * Creates a new TrackController with the given hash and debugging
-	 * flag. A zero hash will result in a unique hash begin generated.
-	 */
-    TrackController(word hash, boolean debug);
-
+public:
+    TrackController(word aHash, boolean aDebug = false)
+    : mHash(aHash), mDebug(aDebug)
+    { }
+   
     /**
-     * Is called when a TrackController is being destroyed. Does the
-     * necessary cleanup. No need to call this manually.
+     * Initialises the TrackController with the MCP_CAN object used
+     * for communication over the CAN-Bus Shield.
      */
-    ~TrackController();
-
-    /**
-     * Initializes the TrackController with the given values. This
-     * should be called before begin, otherwise it will not take
-     * effect. A zero hash will result in a unique hash begin
-     * generated.
-     */
-    void init(word hash, boolean debug, boolean loopback);
-
-    /**
-     * Queries the hash used by the TrackController.
-     */
-    word getHash();
-
-    /**
-     * Reflects whether the TrackController is in debug mode,
-     * where all messages are dumped to the Serial console.
-     */
-    boolean isDebug();
-
-    /**
-     * Reflects whether the TrackController is in debug mode,
-     * where all messages are reflected by the CAN controller.
-     */
-    boolean isLoopback();
-
+    void init(MCP_CAN& aCAN);
+   
     /**
      * Sends a message and reports true on success. Internal method.
      * Normally you don't want to use this, but the more convenient
@@ -275,21 +207,7 @@ class TrackController {
      * may be the same object.
      */
     boolean exchangeMessage(TrackMessage &out, TrackMessage &in,  word timeout);
-
-    /**
-     * Initializes the CAN hardware and starts receiving CAN
-     * messages. CAN messages are put into an internal buffer of
-     * limited size, so they don't get lost, but you have to take
-     * care of them in time. Otherwise the buffer might overflow.
-     */
-    void begin();
-
-    /**
-     * Stops receiving messages from the CAN hardware. Clears
-     * the internal buffer.
-     */
-    void end();
-
+  
     /**
      * Controls power on the track. When passing false, all
      * locomotives will stop, but remember their previous directions
@@ -429,205 +347,12 @@ class TrackController {
      */
     boolean getVersion(byte *high, byte *low); 
 
-};
-
-/**
- * The little brother of TrackController. This class talks to an IR
- * connector box. The interface is more or less a subset of the one
- * that TrackController offers. There are exactly four locomotives,
- * numbered 1 to 4, and sixteen turnouts, numbered 1 to 16. The IR
- * controller does not care about protocols, although in reality
- * it's all MM2. Also, no object-oriented mumbo-jumbo here to
- * accomodate for the limited Arduino resources. Note that with
- * the exception of remembering power this class is stateless,
- * so the caller has to keep track of things, if necessary.
- */
-class TrackControllerInfrared {
-
-    private:
-	
-	/**
-	 * Whether the power is on.
-	 */
-	boolean mPower;
-	
-	/**
-	 * The current value of the toggle bit (needed for RC5).
-	 */
-	word mToggle;
-	
-    public:
-
-	/**
-	 * Creates a new TrackControllerInfrared and does some
-	 * initializing. Assumes the IR LED is on pin 9.
-	 */
-    TrackControllerInfrared();
-
-    /**
-     * Sends a message consisting of address and command (in the
-     * sense of RC5). Takes care of the extension and alternating
-     * bits. Internal method. Normally you don't want to use this,
-     * but the more convenient methods below instead.
-     */
-    boolean sendMessage(word address, word command);
-
-    /**
-     * Controls power on the track. When passing false, all
-     * locomotives will stop, but remember their previous directions
-     * and speeds. When passing true, all locomotives will regain
-     * their old directions and speeds. The system starts in
-     * stopped mode in order to avoid accidents. The return value
-     * reflects whether the call was successful.
-     */
-    boolean setPower(boolean power);
-    
-    /**
-     * Toogles the direction of the given locomotive. This includes
-     * a full stop of the locomotive. The return value reflects whether
-     * the call was successful.
-     */
-    boolean toggleLocoDirection(int loco);
-    
-    /**
-     * Accelerates the given locomotive by 1 level on a scale assuming
-     * a total of 14 speed levels. Does not have any effect if the
-     * maximum speed has already been reached.
-     */
-    boolean accelerateLoco(int loco);
-
-    /**
-     * Accelerates the given locomotive by 1 level on a scale assuming
-     * a total of 14 speed levels. Does not have any effect if the
-     * maximum speed has already been reached.
-     */
-    boolean decelerateLoco(int loco);
-    
-    /**
-     * Toggles the given function of the given locomotive (or simply a
-     * function decoder). Valid functions are 0 to 4, with 0 normally
-     * denoting the head/backlight. The return value reflects whether
-     * the call was successful.
-     */
-    boolean toggleLocoFunction(int loco, int function);
-    
-    /**
-     * Switches a turnout. The return value reflects whether the call
-     * was successful.
-     */
-    boolean setTurnout(int turnout, boolean straight);
-
-};
-
-// ===================================================================
-// === TrackReporterS88 ==============================================
-// ===================================================================
-
-/**
- * Implements the S88 bus protocol for reporting the state of tracks.
- * S88 is basically a long shift register where each bit corresponds
- * to a single contact on the track. Flip-flops on each S88 board make
- * sure activations are stored, so it is not necessary to query a
- * contact at the exact time it is activated. This implementation
- * allows a maximum of 512 bits or 32 full-width (16 bit) S88 boards.
- * The S88 standard recommends a maximum of 30 boards, so we should be
- * on the safe side.
- */
-class TrackReporterS88 {
-
-  private:
-
-  /**
-   * The number of contacts available.
-   */
-  int mSize;
-
-  /**
-   * The most recent contact values we know.
-   */ 
-  byte mSwitches[64];
-
-  public:
-
-  /**
-   * Creates a new TrackReporter with the given number of modules
-   * being attached. While this value can be safely set to the
-   * maximum of 32, it makes sense to specify the actual number,
-   * since this speeds up reporting. The method assumes 16 bit
-   * modules. If you use 8 bit modules instead (or both) you need
-   * to do the math yourself.
-   */
-  TrackReporterS88(int modules);
-  
-  /**
-   * Reads the current state of all contacts into the TrackReporter
-   * and clears the flip-flops on all S88 boards. Call this method
-   * periodically to have up-to-date values.
-   */
-  void refresh();
-
-  /**
-   * Returns the state of an individual contact. Valid index values
-   * are 1 to 512.
-   */
-  boolean getValue(int index);
-
-};
-
-// ===================================================================
-// === TrackReporterIOX ==============================================
-// ===================================================================
-
-/**
- * Implements a low-cost track reporting mechanism based on I/O
- * expanders. Currently the MCP 23S08 and MCP 23S17 are supported,
- * with the MCP 23S08 being treaded just like the 16 bit module, so
- * the upper 8 bits are undefined. The IO expanders are connected via
- * SPI. Pin x is being used for chip select. The interrupt line must
- * be connected to Arduino pin 3 (interrupt 1) and pulled up via a
- * resistor. Multiple expanders can be combined, assuming they are
- * configured to different addresses via the hardware pins. All need
- * to share the interrupt line, which is configured for open drain.
- */
-class TrackReporterIOX {
 
 private:
-
-  /**
-   * The number of IO expanders being used.
-   */
-  int mCount;
-
-  /**
-   * The most recent contact values we know.
-   */ 
-  byte mSwitches[16];
-
-public:
-
-  /**
-   * Creates a new TrackReporter with the given number of expanders.
-   */
-  TrackReporterIOX(int modules);
-  
-  /**
-   * Is called when a TrackReporter is being destroyed. Does the
-   * necessary cleanup. No need to call this manually.
-   */
-  ~TrackReporterIOX();
-
-  /**
-   * Reads the current state of all expanders into the TrackReporter.
-   * Call this method periodically to have up-to-date values.
-   */
-  void refresh();
-
-  /**
-   * Returns the state of an individual contact. Note counting starts
-   * from 1 in order to stay compatible with the S88 reporter.
-   */
-  boolean getValue(int index);
-
+   MCP_CAN* mCAN = nullptr;
+   word mHash = 0;
+   boolean mDebug = false;
 };
+
 
 #endif
